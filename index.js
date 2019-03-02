71c737d3538d18bcb99c5a227c78e2c7a320d2fe
Jangwon <wkddnjset@gmail.com>
2019-03-02 20:07:18 +0900

    SQLite

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { YellowBox } from 'react-native';
+import {AppRegistry} from 'react-native'
+import App from './App'
+import {name as appName} from './app.json'
+import { YellowBox } from 'react-native'
+import SQLite from 'react-native-sqlite-storage'
+
+function okCallback() {
+    console.log("okCallback")
+}
+function errorCallback() {
+    console.log("errorCallback")
+}
+SQLite.openDatabase({name : "testDB", createFromLocation : 1}, okCallback,errorCallback);
+
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 AppRegistry.registerComponent(appName, () => App);
